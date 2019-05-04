bfd450fd301f5e8d129eb6274ef95b5f9a5043de
BartBros <bb.aambroziak@gmail.com>
2019-05-04 10:09:03 +0200

    Add answers check

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
-const answer = ['tak', 'css', 'tak', 'dobra'];
+const questionsText = [
+    'Czy trawa jest zielona?',
+    'Jak się nazywa język, za którego pomocą określimy wygląd strony?',
+    'Czy jest to quiz stworzony w DOM?',
+    'Wybierz prawidłowy przykład użycia funkcji w JavaScript'
+];
+const correctAnswers = ['tak', 'css', 'tak', 'dobra'];
+const userAnswers = [];
 
+const input = document.querySelectorAll('.input-answer');
 const questions = document.querySelectorAll('.question');
 const button = document.getElementById('next');
-const question = document.querySelector('.question');
+const final = document.querySelector('#final');
+var finalMessageGood = '';
+var finalMessageWrong = '';
+
 let q = 0;
 
+// Answers check
+function checkAnswers() {
+    for (let i = 0; i < questionsText.length; i += 1) {
+        if (correctAnswers[i] === userAnswers[i]) {
+            finalMessageGood += '<p>' + questionsText[i] + '</p>';
+        }   else {
+            finalMessageWrong += '<p>' + questionsText[i] + '</p>';
+        }
+    }
+}
+
 button.addEventListener('click', () => {
-    questions[q].style.display = 'none';
-    q += 1;
-    questions[q].style.display = 'block';
+    if (q == 3) { // Last Question
+        userAnswers.push( input[q].value );
+        button.style.display = 'none';
+        questions[q].style.display = 'none';
+        final.style.display = 'block';
+        checkAnswers();
+    } else { // First Question
+        userAnswers.push( input[q].value );
+        questions[q].style.display = 'none';
+        q += 1;
+        questions[q].style.display = 'block';
+        }
 });
+
+
